277cf0d22cf4017075ae5d48448af5e40ccf0689
aniketjha1729 <kumaraniket1729@gmail.com>
2022-04-02 14:52:29 +0530

    ui fix

--- src/components/RoomCard.jsx
+++ src/components/RoomCard.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import Avatar from "@material-ui/core/Avatar";
+import { MdPeopleAlt } from "react-icons/md";
+
 const RoomCard = ({ room }) => {
   const history = useHistory();
   return (
     <div onClick={() => history.push(`/room/${room.id}`)} className="cards">
-      {room.topic}
+      <div className="roomName">{room.topic}</div>
+      <div className="speakerName">
+        <Avatar alt="Remy Sharp" /> &nbsp; John Doe
+      </div>
+      <div className="participants">
+        <MdPeopleAlt/>
+      </div>
     </div>
   );
 };
